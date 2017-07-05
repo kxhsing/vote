d8b83a371f9e954f2b340bd7057614ee1e73c5c0
Karen Hsing <karenhsing@karens-mbp.gateway.sonic.net>
2017-07-05 11:16:29 -0700

    Set up and initial tests

---
+++ test/immutable_spec.js
@@ -0,0 +1,38 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('immutability', () => {
+
+    // describe('A List', () => {
+    //     function addMovie(currentState, movie) {
+    //         return currentState.push(movie);
+    //     }
+        describe('a tree', () => {
+            function addMovie(currentState, movie) {
+                return currentState.update(
+                    'movies', movies => movies.push(movie));
+            }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                'Trainspotting',
+                '28 Days Later',
+                'Sunshine'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                'Trainspotting',
+                '28 Days Later'
+                )
+            }));
+        });
+    });
+});
+
