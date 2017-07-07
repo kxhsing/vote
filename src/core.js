953c413689afc55ea3bfcec9f4849b666fb99b5d
Karen Hsing <karenhsing@karens-mbp.attlocal.net>
2017-07-07 00:32:00 -0700

    Added core spec and core js files. Tests and code to start the vote

---
+++ src/core.js
@@ -0,0 +1,14 @@
+import {List} from 'immutable';
+
+export function setEntries(state, entries) {
+    return state.set('entries', entries);
+}
+
+export function next(state) {
+    const entries = state.get('entries');
+    return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+    });
+}
+
